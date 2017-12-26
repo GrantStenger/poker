91c06f354a5c4e73abbec3a902658fd99228d5a1
Grant Stenger <ghstenge@usc.edu>
2017-12-25 21:00:36 -0800

    Can now detect pairs, flushes, full houses, etc.

--- poker.py
+++ poker.py
@@ -5,6 +5,7 @@ Separate poker.py into several files
 
 import numpy as np
 import os
+from collections import Counter
 
 """ Classes """
 class Deck:
@@ -47,6 +48,9 @@ class Card:
 	def __lt__(self, other_card):
 		numbers = [2,3,4,5,6,7,8,9,10,'Jack','Queen','King','Ace']
 		return numbers.index(self.number) < numbers.index(other_card.number)
+	def __le__(self, other_card):
+		numbers = [2,3,4,5,6,7,8,9,10,'Jack','Queen','King','Ace']
+		return numbers.index(self.number) <= numbers.index(other_card.number)
 
 class PrivateHand:
 	def __init__(self, name):
@@ -66,9 +70,14 @@ class PrivateHand:
 
 	def analyze(self):
 		if self.cards[0].number == self.cards[1].number:
-			print("You have a pair!")
+			print("(Pair!)")
 		else:
-			print("This is not a pair.")
+			print("(Not a pair)")
+
+		if self.cards[0].suit == self.cards[1].suit:
+			print("(Same suit)")
+		else:
+			print("(Different suits)")
 
 class CommunityCards:
 	def __init__(self):
@@ -109,7 +118,7 @@ def sortCards(cards):
 	for i in range(2, len(cards)):
 		j = 0
 		while j < len(newList):
-			if cards[i] < newList[j]:
+			if cards[i] <= newList[j]:
 				newList.insert(j, cards[i])
 				j = len(newList)
 			else:
@@ -121,14 +130,30 @@ def sortCards(cards):
 	return newList
 
 def analyze(hole, community):
-	cards = hole.cards + community.cards
+	cards = sortCards(hole.cards + community.cards)
 	totalClubs = 0
 	totalDiamonds = 0
 	totalHearts = 0
 	totalSpades = 0
-	flush = False
-	straight = False
+	hasFourOfAKind = False
+	hasFullHouse = False
+	hasThreeOfAKind = False
+	hasTwoPair = False
+	hasPair = False
+	hasFlush = False
+	hasStraight = False
+	fourOfAKinds = 0
+	threeOfAKinds = 0
+	pairs = 0
+	fourOfAKindValue = ""
+	threeOfAKindValues = []
+	threeOfAKindValue = ""
+	fullHouseValue = [] #[ToaK, pair]
+	pairValues = []
+	twoPairValue = [] #[higher, lower]
+	pairValue = ""
 	highCard = cards[0]
+	handRank = ""
 
 	for card in cards:
 		# Count the number of cards per suit
@@ -146,37 +171,103 @@ def analyze(hole, community):
 			highCard = card
 
 	if totalClubs >= 5 or totalDiamonds >= 5 or totalHearts >= 5 or totalSpades >= 5:
-		flush = True
+		hasFlush = True
 
 	# Check for straight
 	longestStraightLength = 1
 	currentStraightLength = 1
-	sortedCards = sortCards(cards)
 	numbers = [2,3,4,5,6,7,8,9,10,'Jack','Queen','King','Ace']
-	for i in range(1, len(sortedCards)):
-		if numbers.index(sortedCards[i-1].number) + 1 == numbers.index(sortedCards[i].number):
+	for i in range(1, len(cards)):
+		if numbers.index(cards[i-1].number) + 1 == numbers.index(cards[i].number):
 			currentStraightLength += 1
 			if currentStraightLength > longestStraightLength:
 				longestStraightLength = currentStraightLength
-		elif numbers.index(sortedCards[i-1].number) == numbers.index(sortedCards[i].number):
+		elif numbers.index(cards[i-1].number) == numbers.index(cards[i].number):
 			currentStraightLength = currentStraightLength
 		else:
 			currentStraightLength = 1
 	if longestStraightLength >= 5:
-		straight = True
+		hasStraight = True
+
+	# Check for Four of a Kind, Three of a Kind, Full House, Two Pair, and Pair
+	# Count the times a number appears in the hand
+	cardsSeen = {cards[0].number : 1}
+	for i in range(1, len(cards)):
+		if cards[i].number in cardsSeen:
+			cardsSeen[cards[i].number] = cardsSeen[cards[i].number] + 1
+		else:
+			cardsSeen[cards[i].number] = 1
+
+	for cardNum in cardsSeen:
+		if 4 == cardsSeen[cardNum]:
+			fourOfAKindValue = cardNum
+			fourOfAKinds += 1
+			hasFourOfAKind = True
+		if 3 == cardsSeen[cardNum]:
+			threeOfAKindValues.append(cardNum)
+			threeOfAKinds += 1
+		if 2 == cardsSeen[cardNum]:
+			pairValues.append(cardNum)
+			pairs += 1
+
+	# Check for four of a kinds is handled above
+
+	# Check all three of a kind situations
+	if threeOfAKinds == 2:
+		fullHouseValue = [threeOfAKindValues[1], threeOfAKindValues[0]]
+		hasFullHouse = True
+	elif threeOfAKinds == 1:
+		if pairs > 0:
+			fullHouseValue = [threeOfAKindValues[0], pairValues[pairs-1]]
+			hasFullHouse = True
+		else:
+			threeOfAKindValue = threeOfAKindValues[0]
+			hasThreeOfAKind = True
+	else:
+		# Check all pair situations
+		if pairs > 1:
+			twoPairValue = [pairValues[pairs-1], pairValues[pairs-2]]
+			hasTwoPair = True
+		elif pairs == 1:
+			pairValue = pairValues[pairs-1]
+			hasPair = True
+
+	# Print results
+	print("Analyze " + str(hole.name))
+	print("Sorted cards: " + str(cards))
+	if hasFourOfAKind:
+		handRank = "Four of a Kind of " + str(fourOfAKindValue) + "s"
+	elif hasFullHouse:
+		handRank = "Full House: " + str(fullHouseValue)
+	elif hasFlush:
+		handRank = "Flush: " + str(hasFlush) # Flush of Diamonds
+	elif hasStraight:
+		handRank = "Straight: " + str(hasStraight)
+	elif hasThreeOfAKind:
+		handRank = "Three of a Kind: " + str(hasThreeOfAKind) + str(threeOfAKindValue)
+	elif hasTwoPair:
+		handRank = "Two Pair: " + str(twoPairValue[0]) + "s and " + str(twoPairValue[1]) + "s"
+	elif hasPair:
+		handRank = "Pair of " + str(pairValue) + "s"
+	else:
+		handRank = "High Card: " + str(highCard)
+		
+	print(handRank)
 
 
-	print("Analyze")
-	print(cards)
-	print("Clubs: " + str(totalClubs))
-	print("Diamonds: " + str(totalDiamonds))
-	print("Hearts: " + str(totalHearts))
-	print("Spades: " + str(totalSpades))
+def dealPlayer(number, deck):
+	player = PrivateHand("Player " + str(number))
+	player.push(deck.deal())
+	player.push(deck.deal())
+	player.print()
 	print()
-	print("Flush: " + str(flush))
-	print("Straight: " + str(straight))
-	print("High Card: " + str(highCard))
-	print(sortCards(cards))
+	player.analyze()
+	print()
+	return player
+
+def winner(p1handRank, p2handRank):
+	print()
+
 
 """ main """
 clear = lambda: os.system('clear')
@@ -189,60 +280,72 @@ if wantToPlay == "y":
 	print("Great! Let me shuffle.")
 
 	# Create a shuffled Deck with all 52 cards
-	d = Deck()
+	deck = Deck()
 	numbers = [2,3,4,5,6,7,8,9,10,'Jack','Queen','King','Ace']
 	suits = ['club','diamond','heart','spade']
 	for j in range(len(suits)):
 		for i in range(len(numbers)):
-			d.push(Card(numbers[i],suits[j]))
-	d.shuffle()
+			deck.push(Card(numbers[i],suits[j]))
+	deck.shuffle()
 
 	print("Here are your cards...")
 	print()
 
 	# Deal Player 1
-	player1 = PrivateHand("Player 1")
-	player1.push(d.deal())
-	player1.push(d.deal())
-	player1.print()
+	player1 = dealPlayer(1, deck)
 	print()
 
-	player1.analyze()
+	# Deal Player 1
+	player2 = dealPlayer(2, deck)
 
-	print()
-
-	fold = input("Would you like to fold? [y/n] ")
-	if fold == "n":
+	fold = input("Would you like to call or fold? [c/f] ")
+	if fold == "c":
 		clear()
 		player1.print()
+		print()
+		player2.print()
 		print()
 
 		# Deal the flop
 		CommunityCards = CommunityCards()
-		CommunityCards.push(d.deal())
-		CommunityCards.push(d.deal())
-		CommunityCards.push(d.deal())
+		CommunityCards.push(deck.deal())
+		CommunityCards.push(deck.deal())
+		CommunityCards.push(deck.deal())
 		CommunityCards.print()
 		analyze(player1, CommunityCards)
+		print()
+		analyze(player2, CommunityCards)
 
-		fold = input("Would you like to fold? [y/n] ")
-		if fold == "n":
+		fold = input("Would you like to call or fold? [c/f] ")
+		if fold == "c":
 			clear()
 			player1.print()
 			print()
+			player2.print()
+			print()
 
 			# Deal the turn
-			CommunityCards.push(d.deal())
+			CommunityCards.push(deck.deal())
 			CommunityCards.print()
 			analyze(player1, CommunityCards)
+			print()
+			analyze(player2, CommunityCards)
 
-			fold = input("Would you like to fold? [y/n] ")
-			if fold == "n":
+			fold = input("Would you like to call or fold? [c/f] ")
+			if fold == "c":
 				clear()
 				player1.print()
 				print()
+				player2.print()
+				print()
 
 				# Deal the river
-				CommunityCards.push(d.deal())
+				CommunityCards.push(deck.deal())
 				CommunityCards.print()
 				analyze(player1, CommunityCards)
+				print()
+				analyze(player2, CommunityCards)
+elif wantToPlay != "n":
+	print("Sorry, I don't understand what that means.")
+
+print("Bye! ")
